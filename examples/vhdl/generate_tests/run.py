1985ef2e4501a6362b02aea9b13db81e42584da5
arnfol <arnfol@mail.ru>
2020-09-17 17:04:44 +0300

    change default example to show the problem

--- examples/vhdl/generate_tests/run.py
+++ examples/vhdl/generate_tests/run.py
@@ -15,6 +15,9 @@ to create test bench output files in location specified by VUnit python runner.
 
 from pathlib import Path
 from itertools import product
+import sys
+import os
+sys.path.append(os.path.dirname('/mnt/c/Users/arnfo/Work/vunit/vunit'))
 from vunit import VUnit
 
 
@@ -64,6 +67,7 @@ VU = VUnit.from_argv()
 LIB = VU.add_library("lib")
 LIB.add_source_files(Path(__file__).parent / "test" / "*.vhd")
 
+LIB.set_sim_option('modelsim.vsim_flags', ['-G dur/param_enable=1'])
 TB_GENERATED = LIB.test_bench("tb_generated")
 
 # Just set a generic for all configurations within the test bench
@@ -71,9 +75,56 @@ TB_GENERATED.set_generic("message", "set-for-entity")
 
 for test in TB_GENERATED.get_tests():
     if test.name == "Test 2":
-        # Test 2 should only be run with signed width of 16
-        generate_tests(test, [True], [16])
-        test.set_generic("message", "set-for-test")
+        # I am trying to achieve two test configurations with following sim_options:
+        # test 'cat' : {'modelsim.vsim_flags': ['-G dur/param_enable=1', '-G dur/param_who=cat',
+        #                                       '-G dur/param_what=fish', '-G dur/param_eats=YES'], }
+        # test 'dog' : {'modelsim.vsim_flags': ['-G dur/param_enable=1', '-G dur/param_who=dog',
+        #                                       '-G dur/param_what=fish', '-G dur/param_eats=NO'], }
+        #
+        # the 'default' configuration is also added to make it easier to see how things work
+
+        # ----------------------------------------------------------------
+        # create two configurations with different options
+        # note: LIB sim_options are always overwritten, here is no overwrite=False option
+        test.add_config(name='cat', sim_options={'modelsim.vsim_flags': ['-G dur/param_who=cat']})
+        test.add_config(name='dog', sim_options={'modelsim.vsim_flags': ['-G dur/param_who=dog']})
+
+        # this one preserves default sim_options, and different options could've been
+        # added with set_sim_option in each test_case if it got overwrite argument:
+        test.add_config(name='default')
+
+        # print test_cases' sim_options
+        print('\nSim options after add_configs:')
+        for testconfig in [x for x in test._test_case.get_configuration_dicts()[0].values()][1:]:
+            print("{} modelsim.vsim_flags : {}".format(testconfig.name, testconfig.sim_options["modelsim.vsim_flags"]))
+
+        # ----------------------------------------------------------------
+        # set same sim_option to all test_cases in test
+        # this work's okay
+        test.set_sim_option('modelsim.vsim_flags', ['-G dur/param_what=fish'], overwrite=False)
+
+        # print test_cases' sim_options
+        print('\nSim options after test set_sim_option:')
+        for testconfig in [x for x in test._test_case.get_configuration_dicts()[0].values()][1:]:
+            print("{} modelsim.vsim_flags : {}".format(testconfig.name, testconfig.sim_options["modelsim.vsim_flags"]))
+
+        # ----------------------------------------------------------------
+        # set different optons to differrent test
+        for testconfig in [x for x in test._test_case.get_configuration_dicts()[0].values()][1:]:
+            # note: Configuration.set_sim_option() does not have overwrite argument
+            if testconfig.name == 'cat':
+                testconfig.set_sim_option('modelsim.vsim_flags', ['-G dur/param_eats=YES'])
+            elif testconfig.name == 'dog':
+                testconfig.set_sim_option('modelsim.vsim_flags', ['-G dur/param_eats=NO'])
+            else:
+                testconfig.set_sim_option('modelsim.vsim_flags', ['-G dur/param_eats=DEFAULT'])
+
+        # print test_cases' sim_options
+        print('\nSim options after configuration set_sim_option:')
+        for testconfig in [x for x in test._test_case.get_configuration_dicts()[0].values()][1:]:
+            print("{} modelsim.vsim_flags : {}".format(testconfig.name, testconfig.sim_options["modelsim.vsim_flags"]))
+
+        print()
     else:
         # Run all other tests with signed/unsigned and data width in range [1,5[
         generate_tests(test, [False, True], range(1, 5))
